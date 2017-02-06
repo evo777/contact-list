9718a2487d152405a41b98217a6154559ba5a27e
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-06 01:10:54 -0800

    Rendered AddForm component

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,10 +1,13 @@
 var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStore');
+var AddForm = require('./AddForm.js');
 
 var App = React.createClass({
   render: function() {
     return (
       <div>
-        Hi
+        <AddForm />
       </div>
     )
   }
