f4bf80f37b52180250c5c1c406d858449fbff47a
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-06 13:35:08 -0800

    Implemented initial state

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -3,6 +3,14 @@ var AppActions = require('../actions/AppActions');
 var AppStore = require('../stores/AppStore');
 var AddForm = require('./AddForm.js');
 
+function getAppState(){
+  return {
+    //Coming from AppStore
+    contacts: AppStore.getContacts(),
+    contactToEdit: AppStore.getContactToEdit()
+  }
+}
+
 var App = React.createClass({
   render: function() {
     return (
