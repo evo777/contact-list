4883105d8fa93f535c6d90a9c9dbab07dad2835e
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-06 01:19:42 -0800

    Created add form buttons and fields

--- src/js/components/AddForm.js
+++ src/js/components/AddForm.js
@@ -3,7 +3,27 @@ var AppActions = require('../actions/AppActions');
 var AppStore = require('../stores/AppStore');
 
 var AddForm = React.createClass({
-  render: function() {}
+  render: function() {
+    return(
+      //Class of "well" gives it a grey background
+      //onSubmit is an event handler
+      <div className="well">
+        <h3>Add Contact</h3>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <input type="text" ref="name" className="form-control" placeholder="Please add contact name" />
+          </div>
+          <div className="form-group">
+            <input type="text" ref="phone" className="form-control" placeholder="Please add contact's phone" />
+          </div>
+          <div className="form-group">
+            <input type="text" ref="email" className="form-control" placeholder="Please add contact's email" />
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    );
+  }
 });
 
 module.exports = AddForm;
