ec67799291a05e9acb4c8cc2496d783bd4c6c98e
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-05 19:36:47 -0800

    Implemented dipatcher

---
+++ src/js/dispatcher/AppDispatcher.js
@@ -0,0 +1,18 @@
+var Dispatcher = require('flux').Dispatcher;
+var assign = require('object-assign');
+
+//Including the flux module
+
+var AppDispatcher = assign(new Dispatcher(),{
+  handleViewAction: function(action){
+    var payload = {
+      //Includes the action
+      source: 'VIEW_ACTION',
+      //Passes the action as action
+      action: action
+    };
+    this.dispatch(payload);
+  }
+});
+
+module.exports = AppDispatcher;
