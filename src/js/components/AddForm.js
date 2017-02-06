2afee03b52fa44db291cc2af4b1400badc90b706
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-06 01:12:16 -0800

    Added AddForm component

---
+++ src/js/components/AddForm.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStore');
+
+var AddForm = React.createClass({
+  render: function() {}
+});
+
+module.exports = AddForm;
