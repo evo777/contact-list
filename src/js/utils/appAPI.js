6863b7ba8de36bf9e3f319550befa0bc1ff67f4a
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-06 13:45:16 -0800

    Updated api file with retrieving contact from Firebase functionality

--- src/js/utils/appAPI.js
+++ src/js/utils/appAPI.js
@@ -3,11 +3,30 @@ var AppActions = require('../actions/AppActions');
 
 //Interacting with Firebase
 module.exports = {
-  saveContact: function(contact){
+  saveContact: function(contact) {
     //When saving contacts to firebase, always reference the firebase
     this.firebaseRef = new Firebase('https://contactlist-a4387.firebaseio.com/contacts');
     this.firebaseRef.push({
       contact: contact
+    });
+  },
+
+  getContacts: function() {
+    //When retrieving contacts from firebase, always refernce the database
+    this.firebaseRef = new Firebase('https://contactlist-a4387.firebaseio.com/contacts');
+    this.firebaseRef.once("value", function(snapshot){
+      var contacts = [];
+      snapshot.forEach(function(childSnapshot){
+        var contact = {
+          id: childSnapshot.key(),
+          name: childSnapshot.val().contact.name,
+          phone: childSnapshot.val().contact.phone,
+          email: childSnapshot.val().contact.email
+        }
+        contacts.push(contact);
+        //Going to receive all the contacts that is fetched
+        AppActions.receiveContacts(contacts);
+      });
     });
   }
 }
