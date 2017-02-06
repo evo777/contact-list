b3bb84f548d4d0fc6d30e43c17eac1f7b02528d6
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-06 01:07:24 -0800

    Implemented saveContact function

--- src/js/actions/AppActions.js
+++ src/js/actions/AppActions.js
@@ -2,6 +2,13 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 
 //Holding the functions
 var AppActions = {
+  saveContact: function(contact) {
+    //Calling AppDispatcher
+    AppDispatcher.handleViewAction({
+      actionType: AppConstants.SAVE_CONTACT,
+      contact: contact
+    });
+  }
 }
 
 module.exports = AppActions;
