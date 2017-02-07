485e2f06424258b05c933c6eaa6218cbd0d5aee2
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-06 16:04:00 -0800

    Implemented handleSubmit function

--- src/js/components/EditForm.js
+++ src/js/components/EditForm.js
@@ -9,15 +9,15 @@ var EditForm = React.createClass({
       //onSubmit is an event handler
       <div className="well">
         <h3>Edit Contact</h3>
-        <form>
+        <form {this.handleSubmit}>
           <div className="form-group">
-            <input type="text" ref="name" value={this.props.contactToEdit.name} className="form-control" placeholder="Please add contact name" />
+            <input type="text" ref="name" onChange={this.handleChange.bind(this, 'name')} value={this.props.contactToEdit.name} className="form-control" placeholder="Please add contact name" />
           </div>
           <div className="form-group">
-            <input type="text" ref="phone" value={this.props.contactToEdit.phone} className="form-control" placeholder="Please add contact's phone" />
+            <input type="text" ref="phone" onChange={this.handleChange.bind(this, 'phone')} value={this.props.contactToEdit.phone} className="form-control" placeholder="Please add contact's phone" />
           </div>
           <div className="form-group">
-            <input type="text" ref="email" value={this.props.contactToEdit.email} className="form-control" placeholder="Please add contact's email" />
+            <input type="text" ref="email" onChange={this.handleChange.bind(this, 'email')} value={this.props.contactToEdit.email} className="form-control" placeholder="Please add contact's email" />
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
@@ -32,6 +32,21 @@ var EditForm = React.createClass({
     selected.name = newState;
     //When clicked on Edit button, the contact's information will display on the form for editing
     this.setState({selected: selected});
+  },
+
+  handleSubmit: function(e){
+    e.preventDefault();
+
+    var contact = {
+      id: this.props.contactToEdit.id,
+      //Getting the name from the input box
+      name: this.refs.name.value.trim(),
+      //Has to match with the ref attribute from the form
+      phone: this.refs.phone.value.trim(),
+      email: this.refs.email.value.trim()
+    }
+
+    AppActions.updateContact(contact);
   }
 });
 
